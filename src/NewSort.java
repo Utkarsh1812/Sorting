b5f62f3de9a1c2eccdbc08a6c6f24ed8ea327014
utkarshpatidar1812 <utkarshpatidar1812@localhost>
2022-09-19 09:41:34 +0530

    New Sort

--- src/NewSort.java
+++ src/NewSort.java
@@ -1,9 +1,29 @@
+class sorting {
+    
+    public static void bubbleSort(int [] sort_arr, int len){
+        
+        for (int i=0;i<len-1;++i){
 
-public class NewSort {
+            for(int j=0;j<len-i-1; ++j){
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+                if(sort_arr[j+1]<sort_arr[j]){
 
-	}
+                    int swap = sort_arr[j];
+                    sort_arr[j] = sort_arr[j+1];
+                    sort_arr[j+1] = swap;
 
+                }
+            }
+        }
+    }
+    
+    public static void main( String args[] ) {
+        int [] array = {10,5,3,1,24,12};
+        int len = array.length;
+        bubbleSort(array,len);
+
+        for(int i = 0; i<len; ++i){
+            System.out.print(array[i] + " ");
+        }
+    }
 }
